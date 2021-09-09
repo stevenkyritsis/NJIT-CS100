d40670e1d538f6bfebf41f0048529c2b39f2a1c6
Steven <16929888+stevenkyritsis@users.noreply.github.com>
2021-09-09 17:18:16 -0400

    Update HW1_StevenKyritsis.py

--- HW1_StevenKyritsis.py
+++ HW1_StevenKyritsis.py
@@ -4,3 +4,81 @@ Steven Kyritsis CS100
 September 9, 2021
 '''
 
+#b
+hours = 24
+minutes = 60
+seconds = 60
+
+#c 
+hourlyPay = 7.25
+tax = 0.03
+rent = 250.95
+
+#d
+say1 = 'Hi there'
+say2 = 'How are you'
+say3 = 'Good, and you?'
+'''
+#Exercise 1.1
+#1
+in a print statement if you forget a paranthese or both it will have a syntax error
+
+#2
+if you are trying to print a string and forget the quotes it will throw a syntax error
+
+#3
+it still outputs the correct sum
+
+#4
+it throws a syntax error
+
+#5
+if you have two values with no operator it will throw a syntax error
+
+#Exercise 1.2
+#1
+2562 seconds
+
+#2
+6.25
+
+#3
+#In Seconds
+average = 0.0063
+
+#in Minutes
+average =0.38
+
+#MPH
+average = 22.6
+
+#Exercise 2.1
+SyntaxError: cannot assign to literal
+
+x = y = 1 #works
+
+Python interprets the semicolon
+
+Putting a . after a line
+SyntaxError: invalid syntax
+
+xy = NameError: name 'xy' is not defined
+
+#Exercise 2.2
+#1
+(4/3)*3.14*(5**3) = 523.3333333333334
+
+#2
+>>> book1 = (24.95-(24.95 * 0.4)) + 3
+>>> book2 = (24.95-(24.95 * 0.4)) + 0.75
+>>> sum = book1 + (59 * book2)
+>>> print(sum)
+1116.5357142857144
+
+#3
+>>> mile1 = 52+8.25
+>>> mile2 = mile1 + 7.2
+>>> print(mile2)
+67.45
+
+'''
