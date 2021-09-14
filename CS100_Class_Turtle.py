472bda553b0c0965e69a4d3e07e77995b2e20438
Steven <16929888+stevenkyritsis@users.noreply.github.com>
2021-09-14 15:43:43 -0400

    Create CS100_Class_Turtle.py

---
+++ CS100_Class_Turtle.py
@@ -0,0 +1,20 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.width(20)
+t.circle(200)
+t.up()
+t.left(90)
+t.fd(250)
+t.left(90)
+t.fd(70)
+t.rt(45)
+t.down()
+t.fd(40)
+t.backward(80)
+t.fd(40)
+t.rt(90)
+t.fd(40)
+t.backward(80)
+
