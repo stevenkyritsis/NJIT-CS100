c955ab3353a1e2fc0ba39150cf4330bb2b0b2708
Steven <16929888+stevenkyritsis@users.noreply.github.com>
2021-09-14 15:43:39 -0400

    Create CS100_Class_Notes.py

---
+++ CS100_Class_Notes.py
@@ -0,0 +1,18 @@
+'''
+Turtle
+'''
+import turtle
+
+t = turtle.Turtle()
+
+t.fd(100)
+t.forward(100)
+t.backward(100)
+t.rt(90)
+t.fd(100)
+t.rt(180)
+t.width(20)
+t.fd(200)
+t.color('papaya whip')
+t.circle(100)
+
